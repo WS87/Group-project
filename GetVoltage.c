d4d9813f4ef823d309d2935882d0b1ece4e88de2
12UsmanAhmad <99757462+12UsmanAhmad@users.noreply.github.com>
2022-02-25 17:04:27 +0000

    Update GetVoltage.c

--- GetVoltage.c
+++ GetVoltage.c
@@ -2,54 +2,99 @@
 #include "Board_LED.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "M:\Year 2\Design construction and test\Lab 3\PB_LCD_Drivers.c"
+#include <string.h>
 #include "M:\Year 2\Design construction and test\Lab 3\PB_LCD_Drivers.h"
 
-void VoltmeterConfig()
+uint32_t volatile ticks = 0;
+
+void SysTick_Handler(void)
 {
-	// RCC ->APB1ENR |= RCC_APB1ENR_TIM6EN; //enables clock up only 
-	// RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN; //enables GPIO clock
-	//setting GPIO to read the analouge signal
-	// RCC ->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	
-	// Sets mode register 14 to analog mode
-	GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER14) | (0x01 << GPIO_MODER_MODER14_Pos); 
-	
-	RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; // Enable ADC preipheral clock
-	ADC1->CR2 |= ADC_CR2_ADON; // Enable ADC
-	ADC1->CR1 |= ADC_CR1_DISCEN; // Set ADC to discontinuous mode
-	ADC1->CR2 |= ADC_CR2_EOCS; // Enable end-of-conversion flag
+	ticks++;
 }
 
-volatile uint32_t ADCconv;
-char ADCconvString[10];
+void waitForATime(int howLongToWait)
+{
+	uint32_t whenStarted = ticks;
+	while (ticks - whenStarted < howLongToWait);
+}
+
+double ConvertADCValue(uint32_t value)
+{
+	double ret;
+	ret = (value + 1.5);
+	return ret;
+}
+
+void ADC_Config()
+{
+	// Enable GPIO-C clock
+	RCC->AHB1ENR = (RCC->AHB1ENR & ~RCC_AHB1ENR_GPIOCEN_Msk) | (0x1 << RCC_AHB1ENR_GPIOCEN_Pos);
+	// Enable ADC1 clock
+	RCC->APB2ENR = (RCC->APB2ENR & ~RCC_APB2ENR_ADC1EN_Msk) | (0x1 << RCC_APB2ENR_ADC1EN_Pos);
+	// Sets mode register 14 to analog mode
+	GPIOC->MODER = (GPIOC->MODER & ~GPIO_MODER_MODER4_Msk) | (0x3 << GPIO_MODER_MODER4_Pos); 
+	// Set ADC to discontinuous mode
+	ADC1->CR1 = (ADC1->CR1 & ~ADC_CR1_DISCEN_Msk) | (0x1 << ADC_CR1_DISCEN_Pos);
+	// Set ADC to perform one conversion before raising EOC flag
+	ADC1->SQR1 = (ADC1->SQR1 & ~ADC_SQR1_L_Msk) | (0x0 << ADC_SQR1_L_Pos);
+	// Set ADC to read input 14
+	ADC1->SQR3 = (ADC1->SQR3 & ~ADC_SQR3_SQ1_Msk) | (0xE << ADC_SQR3_SQ1_Pos);
+	// Enable ADC :D
+	ADC1->CR2 = (ADC1->CR2 & ~ADC_CR2_ADON_Msk) | (0x1 << ADC_CR2_ADON_Msk);
+}
 
 int main(void)
 {
 	SystemCoreClockUpdate();
-	SysTick_Config(SystemCoreClock/2);
-	
+	SysTick_Config(SystemCoreClock / 2);
+
 	// Initialise LCD
 	PB_LCD_Init();
 	PB_LCD_Clear();
-	
+
 	// Initialise ADC
-	VoltmeterConfig();
+	ADC_Config();
+	
+	// Temporary variables to store ADC output
+	uint32_t ADCconv;
+	uint16_t y;
+	double ADCfinal;
 	
 	// Main loop
-	while(1)
+	while (1)
 	{
+		int finished = 0;
+		
 		// Begin a conversion with the ADC
-		ADC1->CR2 |= ADC_CR2_SWSTART;
+		ADC1->CR2 = (ADC1->CR2 & ~ ADC_CR2_SWSTART_Msk) | (0x1 << ADC_CR2_SWSTART_Pos);
 		
-		// Wait until the conversion is complete
-		while (!(ADC1->CR2 & ADC_SR_EOC));
-		
-		// Store the voltage in a local variable
-		ADCconv = ADC1->DR;
-		
-		// Store ADCconv value as a string in ADCconvString
-		sprintf(ADCconvString, "%u", ADCconv);
-		PB_LCD_WriteString(ADCconvString, 10);
+		while (finished == 0)
+		{
+			y = ADC1->SR;
+			if ((y & ADC_SR_EOC_Msk) == ADC_SR_EOC_Msk)
+			{
+				// START WAIT
+				int i = 0;
+				while (i < 1000) i++;
+				i = 0;
+				// END WAIT
+				
+				ADCconv = ADC1->DR;
+				ADCfinal = ConvertADCValue(ADCconv);
+				char ADCconvString[8];
+				
+				waitForATime(1);
+				
+				PB_LCD_Clear();
+				
+				strcpy(ADCconvString, "00000000");
+				
+				snprintf(ADCconvString, 10, "%.3f", ADCfinal);
+				
+				PB_LCD_WriteString(ADCconvString, 10);
+				
+				finished = 1;
+			}
+		}
 	}
 }
